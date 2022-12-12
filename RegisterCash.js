407a6114a247a8bbc61013c82d15cbe86a85c9d5
Ivan_Thanon <ivanmoreno199518@gmail.com>
2022-12-12 23:39:33 +0000

    Update RegisterCash.js

--- RegisterCash.js
+++ RegisterCash.js
@@ -1,46 +1,46 @@
 function checkCashRegister(price, cash, cid) {
-    let devolver = cash-price;
-    let suma = 0
+    let change = cash-price;
+    let money = 0
     let conversion = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100]
     let map = new Map()
   
-    let cambio = {
+    let finalChange = {
     status: "",
     change: new Array()
     }
   
     for (var i = 0; i < cid.length; i++) {
-        suma += cid[i][1];
+        money += cid[i][1];
     }
   
-    if (devolver > 0 && devolver != suma) {
+    if (change > 0 && change != money) {
         for (var i = cid.length-1; i>=0; i--) {
-            let sumamonedas = 0;
-            while (devolver >= conversion[i] && cid[i][1] != 0) {
-               devolver = Math.round((devolver - conversion[i])*100) / 100
+            let moneymonedas = 0;
+            while (change >= conversion[i] && cid[i][1] != 0) {
+               change = Math.round((change - conversion[i])*100) / 100
                cid[i][1] = cid[i][1] - conversion[i]
-               sumamonedas += conversion[i]
-               map.set(cid[i][0],sumamonedas) 
+               moneymonedas += conversion[i]
+               map.set(cid[i][0],moneymonedas) 
             }
         }
     }
     
     let counter = 0;
-    for (let clave of map) {
-        cambio.change[counter] = clave
+    for (let key of map) {
+        finalChange.change[counter] = key
         counter++;
     } 
 
-    if (suma == devolver) {
-     cambio.status = "CLOSED";
-     cambio.change = cid;
-    } else if (devolver > 0 || suma < devolver) {
-     cambio.change = [];
-     cambio.status = "INSUFFICIENT_FUNDS"; 
+    if (money == change) {
+     finalChange.status = "CLOSED";
+     finalChange.change = cid;
+    } else if (change > 0 || money < change) {
+     finalChange.change = [];
+     finalChange.status = "INSUFFICIENT_FUNDS"; 
     } else {
-     cambio.status = "OPEN";
+     finalChange.status = "OPEN";
     }
-    return cambio;
+    return finalChange;
 }
 
 checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
